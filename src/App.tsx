283ac480941b5f3dd9313004e7d7f1fd03edb1da
Айрат Жаншуаков <codev0@Ajrats-Mac-mini.local>
2024-09-19 00:23:28 +0500

    add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { reatomComponent } from "@reatom/npm-react";
 import { reatomProfile } from "./profile";
+import { ErrorBoundary } from "react-error-boundary";
 
 const App = reatomComponent(({ ctx }) => {
   const model = ctx.spy(reatomProfile);
 
   return (
-    <>
-      <h1>Profile</h1>
-      {ctx.spy(model.profileResource.statusesAtom) && <div>Loading...</div>}
-      {ctx.spy(model.profileResource.errorAtom) && <div>Error</div>}
-      {ctx.spy(model.profileResource.dataAtom) && (
-        <div>
-          <p>Username: {ctx.spy(model.profileResource.dataAtom)?.username}</p>
-        </div>
-      )}
-    </>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <>
+        <h1>Profile</h1>
+        {ctx.spy(model.profileResource.statusesAtom) && <div>Loading...</div>}
+        {ctx.spy(model.profileResource.errorAtom) && <div>Error</div>}
+        {ctx.spy(model.profileResource.dataAtom) && (
+          <div>
+            <p>Username: {ctx.spy(model.profileResource.dataAtom)?.username}</p>
+          </div>
+        )}
+      </>
+    </ErrorBoundary>
   );
 }, "App");
 
